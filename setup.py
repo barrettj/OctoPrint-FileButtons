88708a90167eb4206b5cb40cdba34be46dc06ba3
Barrett Jacobsen <barrett@attainmetncompany.com>
2021-02-08 13:19:17 -0600

    url fix

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Barrett Jacobsen"
 plugin_author_email = "dont@example.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/BarrettJ/somethingprobably"
+plugin_url = "https://github.com/BarrettJ/OctoPrint-FileButtons"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
