3659d4cd873392222b5307e86d145675073913b1
Barrett Jacobsen <barrett@attainmetncompany.com>
2021-02-08 19:48:51 -0600

    revert

--- octoprint_filebuttons/__init__.py
+++ octoprint_filebuttons/__init__.py
@@ -14,10 +14,6 @@ class FileButtonsPlugin(octoprint.plugin.StartupPlugin,
         self.centerChannel = 38
         self.rightChannel = 36
 
-        self.leftPressed = False
-        self.centerPressed = False
-        self.rightPressed = False
-
     def on_after_startup(self):
         self._logger.info("FileButtons %s on_after_startup!", self._plugin_version)
         self._logger.info(self._printer.get_state_id())
@@ -25,60 +21,30 @@ class FileButtonsPlugin(octoprint.plugin.StartupPlugin,
         self.setup_GPIO()
 
 
-    def button_rising_callback(self, channel):
-    	self._logger.info("FileButtons rising button callback channel {}".format(channel))
+    def button_callback(self, channel):
+    	self._logger.info("FileButtons button callback channel {}".format(channel))
     	# self._printer.commands("M117 FileButtons - {0}".format(channel))
 
-        if channel == self.centerChannel:
-            if self.centerPressed:
-                return
-            else:
-                self.centerPressed = True
+        if self._printer.is_closed_or_error():
+            return
 
-            if self.leftPressed:
+        if channel == self.centerChannel:
+            if GPIO.input(self.leftChannel):
                 self._printer.commands("M117 Center While Left")
-            elif self.rightPressed:
+            elif GPIO.input(self.rightChannel):
                 self._printer.commands("M117 Center While Right")
             else:
                 self._printer.commands("M117 Center Button")
         elif channel == self.leftChannel:
-            if self.leftPressed:
-                return
-            else:
-                self.leftPressed = True
-
-
-            if self.rightPressed:
+            if  GPIO.input(self.rightChannel):
                 self._printer.commands("M117 Left While Right")
             else:
                 self._printer.commands("M117 Left Button")
         elif channel == self.rightChannel:
-            if self.rightPressed:
-                return
-            else:
-                self.rightPressed = True
-
-
-            if self.leftPressed:
+            if GPIO.input(self.leftChannel):
                 self._printer.commands("M117 Right While Left")
             else:
                 self._printer.commands("M117 Right Button")
-        else:
-            self._printer.commands("M117 Unknown Button")
-
-    def button_falling_callback(self, channel):
-        self._logger.info("FileButtons falling button callback channel {}".format(channel))
-        # self._printer.commands("M117 FileButtons - {0}".format(channel))
-
-        if channel == self.centerChannel:
-            if self.centerPressed:
-                self.centerPressed = False
-        elif channel == self.leftChannel:
-            if self.leftPressed:
-                self.leftPressed = False
-        elif channel == self.rightChannel:
-            if self.rightPressed:
-                self.rightPressed = False
         else:
             self._printer.commands("M117 Unknown Button")
 
@@ -96,8 +62,7 @@ class FileButtonsPlugin(octoprint.plugin.StartupPlugin,
         try:
             if channel != -1:
                 GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-                GPIO.add_event_detect(channel, GPIO.RISING, callback=self.button_rising_callback, bouncetime = 250)
-                GPIO.add_event_detect(channel, GPIO.FALLING, callback=self.button_falling_callback, bouncetime = 250)
+                GPIO.add_event_detect(channel, GPIO.RISING, callback=self.button_callback, bouncetime = 250)
                 self._logger.info("New Event Detect has been added to GPIO # %s", channel)
         except:
             self._logger.exception("Cannot setup GPIO ports %s, check to makes sure you don't have the same ports assigned to multiple actions", str(channel))
